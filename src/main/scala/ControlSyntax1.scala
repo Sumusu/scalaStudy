aad40758d6f46bf73da8c14a6367144c541bb2cd
Susumu Yamada <yamada.susumu@sharp.co.jp>
2018-04-27 10:05:06 +0900

    練習問題を解く。

---
+++ src/main/scala/ControlSyntax1.scala
@@ -0,0 +1,12 @@
+object ControlSyntax1{
+  def main(args: Array[String]): Unit = {
+    var age: Int = 5
+    var isSchoolStarted :Boolean = false
+    if (age > 0 && age < 7 && !isSchoolStarted) {
+      println("幼児です")
+    }else{
+      println("幼児ではありません")
+    }
+
+  }
+}
